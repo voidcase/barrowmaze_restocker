36d3d362f463976a212c40be041c1aecf20eed2d
Isak Lindhé <isak.e.lindhe@gmail.com>
2021-03-14 00:58:57 +0100

    enable spoilers via env

--- api.py
+++ api.py
@@ -1,15 +1,29 @@
 import flask
 from flask import request, Response, render_template
 from restocker import Restocker
+import math
+import os
+
 
 
 api = flask.Flask(__name__)
+try:
+    spoiler_safe = bool(int(os.environ.get('BARROWMAZE_SPOILER_SAFE', default=1)))
+except ValueError:
+    print('bad value for environment variable BARROWMAZE_SPOILER_SAFE. should be 0 or 1')
+    print('setting spoiler_safe to True')
+    spoiler_safe = True
 
-logic = Restocker('./tables', spoiler_safe=True)
+logic = Restocker('./tables', spoiler_safe=spoiler_safe)
 
 @api.route('/restock')
 def restock():
-    lvl = int(request.args.get('lvl', default=1))
+    try:
+        lvl = int(request.args.get('lvl', default=1))
+        lvl = min(max(10, lvl), 1)
+    except ValueError:
+        return "Hello there! Are you trying to pentest my thing by entering weird data?\n" + \
+                "That's nice. Come help me inprove it at https://github.com/voidcase/barrowmaze_restocker :)"
     return Response(logic.roll_traverse_table(party_level=lvl), content_type='text/plain')
 
 @api.route('/')
